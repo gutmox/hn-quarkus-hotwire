2a687de5046bab984eb8219756e47ea427ba8c22
kai strempel <kai.strempel@takeaway.com>
2021-01-30 16:43:54 +0100

    first hotwire parts

--- src/main/java/org/hn/resource/RankingResource.java
+++ src/main/java/org/hn/resource/RankingResource.java
@@ -3,22 +3,21 @@ package org.hn.resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import io.quarkus.qute.TemplateInstance;
 import io.quarkus.qute.api.CheckedTemplate;
 
-@Path("/")
-public class RootResource {
-
+@Path("/ranking")
+public class RankingResource {
     @CheckedTemplate
     public static class Templates {
-        public static native TemplateInstance root(); 
+        public static native TemplateInstance ranking(); 
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get(@QueryParam("name") String name) {
-        return Templates.root().data("name", name);
+    public TemplateInstance get() {
+        return Templates.ranking();
     }
+
 }
