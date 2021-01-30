acda09c2a131f7ff40c584996e7b5f280d8ead70
kai strempel <kai.strempel@takeaway.com>
2021-01-30 10:34:47 +0100

    :tada:

---
+++ src/test/java/org/hn/resource/NativeRootResourceIT.java
@@ -0,0 +1,9 @@
+package org.hn.resource;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeRootResourceIT extends RootResourceTest {
+
+    // Execute the same tests but in native mode.
+}
