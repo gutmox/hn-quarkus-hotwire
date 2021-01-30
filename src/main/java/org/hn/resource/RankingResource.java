fef0092c06e529b6b58eb343bdbca1f356d813eb
kstrempel <kai.strempel@takeaway.com>
2021-01-30 19:46:23 +0100

    SSE is working

--- src/main/java/org/hn/resource/RankingResource.java
+++ src/main/java/org/hn/resource/RankingResource.java
@@ -1,23 +1,26 @@
 package org.hn.resource;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import io.quarkus.qute.TemplateInstance;
-import io.quarkus.qute.api.CheckedTemplate;
+import org.hn.service.HNService;
+import org.jboss.resteasy.reactive.RestSseElementType;
+
+import io.smallrye.mutiny.Multi;
 
 @Path("/ranking")
 public class RankingResource {
-    @CheckedTemplate
-    public static class Templates {
-        public static native TemplateInstance ranking(); 
-    }
+    @Inject
+    HNService service;
 
     @GET
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get() {
-        return Templates.ranking();
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @RestSseElementType("text/vnd.turbo-stream.html")
+    @Path("/stream")
+    public Multi<String> updateStream() {
+        return service.updateStream(25, "Kai");
     }
 
 }
